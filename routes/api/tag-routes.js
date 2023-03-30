76d64d177227f17dfa7580fecae38f41141d5ebf
Hunter Wilson <huntertheguy123@gmail.com>
2023-03-29 22:35:17 -0500

    I added async to all of the tag routes and did two get routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,27 +1,49 @@
 const router = require('express').Router();
+const { json } = require('sequelize/types');
+const { endsWith } = require('sequelize/types/lib/operators');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const allTags = await Tag.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(allTags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const singletag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    });
+    if (!singletag){
+      res.status(404).json({ message: 'No tag with this id'});
+      return;
+    }
+  res.status(200).json(singletag);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
 });
 
