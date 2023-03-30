bea87ef9f4eb1ccbccdceb26b02a922cc02ffcd3
Hunter Wilson <huntertheguy123@gmail.com>
2023-03-30 12:04:34 -0500

    I worked on the update tag route and create new tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { json } = require('sequelize/types');
 const { endsWith } = require('sequelize/types/lib/operators');
 const { Tag, Product, ProductTag } = require('../../models');
+const { update } = require('../../models/Product');
 
 // The `/api/tags` endpoint
 
@@ -37,10 +38,30 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const makeTag = await Tag.create(req.body);
+    res.status(200).json(makeTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!updateTag) {
+      res.status(404).json({ message: "No tag by this id"})
+      return;
+    }
+    res.status(200).json( {message: "Update successful"});
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
